e9dbd62891dd707f14914e862e0cb368475690ff
m-11401374 <115518001+m-11401374@users.noreply.github.com>
2022-10-18 11:19:55 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
 basic.forever(function () {
-    if (input.lightLevel() > 225) {
-    	
+    if (input.isGesture(Gesture.Shake)) {
+        pins.analogWritePin(AnalogPin.P0, 1)
+        basic.pause(200)
+        pins.analogWritePin(AnalogPin.P0, 0)
+        basic.pause(200)
+        pins.analogWritePin(AnalogPin.P0, 1)
+        basic.pause(200)
+        pins.analogWritePin(AnalogPin.P0, 0)
+        basic.pause(200)
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() > 100) {
+        pins.servoWritePin(AnalogPin.P2, 100)
     } else {
-    	
+        pins.servoWritePin(AnalogPin.P2, 0)
     }
 })
